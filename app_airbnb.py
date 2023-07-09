75e472cae0302ec27040ea618e6465793e0443a9
fhouska <fabianahouska@gmail.com>
2023-07-09 20:28:05 +0200

    f

--- app_airbnb.py
+++ app_airbnb.py
@@ -209,11 +209,12 @@ if selected == 'Análisis Exploratorio':
             st.plotly_chart(fig1)
              
         with col2:
+            neighbourhood_count=df['neighbourhood'].value_counts().sort_values(ascending=True)
             dfneighbourhood = pd.DataFrame(neighbourhood_count)
             dfneighbourhood = dfneighbourhood.reset_index()
             adam = gpd.read_file("data/neighbourhoods.geojson")
-            fig2 = px.choropleth_mapbox(dfneighbourhood, geojson=adam, featureidkey='properties.neighbourhood',locations ="neighbourhood",color ='Count', 
-                                        color_continuous_scale='magma', title="Distritos de Estambul",zoom=10, hover_data =['neighbourhood','Count'],
+            fig2 = px.choropleth_mapbox(dfneighbourhood, geojson=adam, featureidkey='properties.neighbourhood',locations ="neighbourhood",color ='count', 
+                                        color_continuous_scale='magma', title="Distritos de Estambul",zoom=10, hover_data =['neighbourhood','count'],
                                         mapbox_style="carto-positron",width=700, height=700,center = {"lat": 41.0036, "lon": 28.9737})
             fig2.update(layout_coloraxis_showscale=True)
             fig2.update_layout(paper_bgcolor="#fff",font_color="#AF1D56",title_font_size=20, title_x = 0.2)
